a6642df2c2ae559c7ce9378921e240c2b6b8b9d5
Cahleeb <calebchristopherparks@gmail.com>
2023-04-14 23:30:52 -0400

    Segregated scripts

---
+++ immSetup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Copy all commands in /usr/bin to trove dir
+trovePath="$thisDirPath/$magicPrefix""trove"
+if $makeImmutable; then
+    mkdir "$trovePath"
+    cp -r /usr/bin/* "$trovePath"
+    $trovePath"/echo" "Trove."
+fi
+
+# Immute script:
+touch immute.sh
+chmod +x immute.sh
+echo "#!/bin/bash" >> immute.sh
+echo "insmod $thisDirPath/$koFile" >> immute.sh
+echo "$trovePath/chattr +i -R /" >> immute.sh
+immutePath="$thisDirPath/immute.sh"
+immuteServicePath="/etc/systemd/system/""$magicPrefix""immute.service"
+touch $immuteServicePath
+echo "[Unit]" >> "$immuteServicePath"
+echo "Description=Joe Mamma" >> "$immuteServicePath"
+echo "After=multi-user.target" >> "$immuteServicePath"
+echo "" >> "$immuteServicePath"
+echo "[Service]" >> "$immuteServicePath"
+echo "Type=simple" >> "$immuteServicePath"
+echo "RemainAfterExit=yes" >> "$immuteServicePath"
+echo "ExecStart=$immutePath" >> "$immuteServicePath"
+echo "TimeoutStartSec=0" >> "$immuteServicePath"
+echo "" >> "$immuteServicePath"
+echo "[Install]" >> "$immuteServicePath"
+echo "WantedBy=default.target" >> "$immuteServicePath" 
+systemctl daemon-reload
+systemctl enable "$immuteServicePath"
+echo "Immute."
+
+# Shutdown script:
+touch shutdown.sh
+chmod +x shutdown.sh
+echo "#!/+bin/bash" >> shutdown.sh
+echo "$trovePath/chattr -i -R /" >> shutdown.sh
+shutdownPath="$thisDirPath/shutdown.sh"
+shutdownServicePath="/etc/systemd/system/""$magicPrefix""shutdown.service"
+touch $shutdownServicePath
+echo "[Unit]" >> "$shutdownServicePath"
+echo "Description=Joe Mamma" >> "$shutdownServicePath"
+echo "DefaultDependencies=no" >> "$shutdownServicePath"
+echo "Before=shutdown.target" >> "$shutdownServicePath"
+echo "" >> "$shutdownServicePath"
+echo "[Service]" >> "$shutdownServicePath"
+echo "Type=oneshot" >> "$shutdownServicePath"
+echo "ExecStart=$shutdownPath" >> "$shutdownServicePath"
+echo "TimeoutStartSec=0" >> "$shutdownServicePath"
+echo "" >> "$shutdownServicePath"
+echo "[Install]" >> "$shutdownServicePath"
+echo "WantedBy=shutdown.target" >> "$shutdownServicePath" 
+systemctl daemon-reload
+systemctl enable "$shutdownServicePath"
+
+echo "Setup."
--- start.sh
+++ start.sh
@@ -21,47 +21,11 @@ if test -f "$koFile"; then
     thisDirPath=$(pwd)
     echo "Hidden."
 
-    # Copy all commands in /usr/bin to trove dir
-    trovePath="$thisDirPath/$magicPrefix""trove"
-    if $makeImmutable; then
-        mkdir "$trovePath"
-        cp -r /usr/bin/* "$trovePath"
-        $trovePath"/echo" "Trove."
-    fi
-
     # Boot script:
     touch boot.sh
     chmod +x boot.sh
     echo "#!/bin/bash" >> boot.sh
     echo "insmod $thisDirPath/$koFile" >> boot.sh
-    if $makeImmutable; then
-        echo "$trovePath/chmod -x /usr/bin/rm" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/cp" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/chmod" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/mkdir" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/nano" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/touch" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/mv" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/rmdir" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/grep" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/egrep" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/sed" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/cat" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/lsattr" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/ls" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/mount" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/dd" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/fdisk" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/fsck" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/parted" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/mkfs" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/lsblk" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/df" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/rsynch" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/stat" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/awk" >> boot.sh
-        echo "$trovePath/chmod -x /usr/bin/vi" >> boot.sh
-    fi
     bootPath="$thisDirPath/boot.sh"
     bootServicePath="/etc/systemd/system/""$magicPrefix""load.service"
     touch $bootServicePath
@@ -81,30 +45,7 @@ if test -f "$koFile"; then
     systemctl enable "$bootServicePath"
     echo "Boot."
 
-    # Shutdown script:
-    if $makeImmutable; then
-        touch shutdown.sh
-        chmod +x shutdown.sh
-        echo "#!/+bin/bash" >> shutdown.sh
-        echo "$trovePath/chmod +x /usr/bin/*" >> shutdown.sh
-        shutdownPath="$thisDirPath/shutdown.sh"
-        shutdownServicePath="/etc/systemd/system/""$magicPrefix""shutdown.service"
-        touch $shutdownServicePath
-        echo "[Unit]" >> "$shutdownServicePath"
-        echo "Description=Joe Mamma" >> "$shutdownServicePath"
-        echo "DefaultDependencies=no" >> "$shutdownServicePath"
-        echo "Before=shutdown.target" >> "$shutdownServicePath"
-        echo "" >> "$shutdownServicePath"
-        echo "[Service]" >> "$shutdownServicePath"
-        echo "Type=oneshot" >> "$shutdownServicePath"
-        echo "ExecStart=$shutdownPath" >> "$shutdownServicePath"
-        echo "TimeoutStartSec=0" >> "$shutdownServicePath"
-        echo "" >> "$shutdownServicePath"
-        echo "[Install]" >> "$shutdownServicePath"
-        echo "WantedBy=shutdown.target" >> "$shutdownServicePath" 
-        systemctl daemon-reload
-        systemctl enable "$shutdownServicePath"
-    fi
+    ./immSetup.sh
 
     echo "Built."
 fi
